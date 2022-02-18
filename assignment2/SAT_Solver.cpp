4f6d81fd5b0bc6e514dec543b60050fdbb37a1ba
AKHILMANISHJAIN <84617145+AKHILMANISHJAIN@users.noreply.github.com>
2022-02-18 16:59:41 +0530

    Update SAT_Solver.cpp

--- assignment2/SAT_Solver.cpp
+++ assignment2/SAT_Solver.cpp
@@ -12,21 +12,47 @@ const int32_t M=1e9+7;
 const int32_t MM=998244353;
 const int32_t N=1e5+1;
 void solve(int tc)
-{ int n,nc;
+{ int n,nc,f=0;
   cin>>n>>nc;
+  vector<int>val;
+  vector<pair<int,int>>sign;
+  for(int i=0;i<n;i++)
+      sign.push_back(make_pair(0,0));
+  for(int i=0;i<n;i++)
+  val.push_back(-1);
   vector<vector<int>>v;
   for(int i=0;i<nc;i++){
       vector<int>v1;
       int c=1;
       while(c){
           cin>>c;
-          if(c)
+          if(c){
           v1.push_back(c);
+          int k=abs(c);
+          if(c>0)
+          sign[k-1].first++;
+          else
+          sign[k-1].second++;
+          }
       }
       v.push_back(v1);
   }
   std::sort(v.begin(),v.end(),[](const vector<int>&a, const vector<int>&b){return a.size()<b.size();})
-  
+  for(int i=0;i<nc && v[i].size()<=1;i++){
+      if(v[i].size()==0)
+      v.erase(v.begin()+i);
+      if(v[i].size()==1){
+          int t=v[i][0];
+          if(t>0 && v[t-1]!=0)
+          v[t-1]=1;
+          else if(t<0 && v[t-1]!=1)
+          v[t-1]=0;
+          else
+          f=1;
+          v.erase(v.begin()+i);
+      }
+  }
+  for()
  }
 int32_t main()
 {
